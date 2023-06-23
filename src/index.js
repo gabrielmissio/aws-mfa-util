a1191ba31332e5191132fa652f84863d33553b6b
gabrielmissio <gabriel.missio@compasso.com.br>
2023-06-22 21:29:42 -0300

    feat: Get session token using AWS SDK v3

--- src/index.js
+++ src/index.js
@@ -1,44 +1,74 @@
 const fs = require('fs')
 const ini = require('ini')
+const { STSClient, GetSessionTokenCommand } = require('@aws-sdk/client-sts')
 
 const args = process.argv.slice(2)
 const options = parseArguments(args)
+const credentials = ini.parse(fs.readFileSync(options.credentialsFilePath, 'utf8'))
 
-const credentialsFile = `${process.env.HOME}/.aws/credentials`
-const credentials = ini.parse(fs.readFileSync(credentialsFile, 'utf8'))
+async function main () {
+  const profile = credentials[options.sourceProfile]
+  if (!profile) {
+    throw new Error(`Profile ${options.sourceProfile} not found`)
+  }
 
-// Fake credentials
-createOrUpdateMFAProfile({
-  AccessKeyId: 'AA11',
-  SecretAccessKey: 'BB22',
-  SessionToken: 'CC33'
-})
+  const stsClient = new STSClient({
+    credentials: {
+      accessKeyId: profile.aws_access_key_id,
+      secretAccessKey: profile.aws_secret_access_key
+    }
+  })
+
+  const sessionToken = await stsClient.send(
+    new GetSessionTokenCommand({
+      SerialNumber: profile.mfa_serial,
+      TokenCode: options.mfaCode
+    })
+  )
+
+  const mfaProfile = createOrUpdateMFAProfile(sessionToken.Credentials)
+  console.log(`New credentials have been saved to the profile ${options.outputProfile}, expires ${mfaProfile.expiration}`)
+}
 
 function createOrUpdateMFAProfile (sessionToken) {
   const mfaProfile = {
     aws_access_key_id: sessionToken.AccessKeyId,
     aws_secret_access_key: sessionToken.SecretAccessKey,
-    aws_session_token: sessionToken.SessionToken
+    aws_session_token: sessionToken.SessionToken,
+    expiration: sessionToken.Expiration.toISOString()
   }
 
-  credentials[`${options.profileTarget}-mfa`] = mfaProfile
+  credentials[options.outputProfile] = mfaProfile
+  fs.writeFileSync(options.credentialsFilePath, ini.stringify(credentials), 'utf8')
 
-  fs.writeFileSync(credentialsFile, ini.stringify(credentials), 'utf8')
+  return mfaProfile
 }
 
 function parseArguments (args) {
   const options = {
-    profileTarget: 'default',
-    mfaCode: null
+    mfaCode: null,
+    sourceProfile: 'default',
+    outputProfile: 'mfa',
+    credentialsFilePath: `${process.env.HOME}/.aws/credentials`
   }
 
   for (let i = 0; i < args.length; i++) {
-    if (args[i] === '-s' && i < args.length - 1) {
-      options.profileTarget = args[i + 1]
-    } else if (args[i] === '-c' && i < args.length - 1) {
+    if (args[i] === '-c' && i < args.length - 1) {
       options.mfaCode = args[i + 1]
+    } else if (args[i] === '-s' && i < args.length - 1) {
+      options.sourceProfile = args[i + 1]
+    } else if (args[i] === '-o' && i < args.length - 1) {
+      options.outputProfile = args[i + 1]
+    } else if (args[i] === '-f' && i < args.length - 1) {
+      options.credentialsFilePath = args[i + 1]
     }
   }
 
   return options
 }
+
+main()
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
